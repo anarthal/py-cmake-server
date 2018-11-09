2158a70aa3700145ead5bcea2b6bd88c87445330
ruben <rubenperez038@gmail.com>
2018-11-09 23:01:10 +0000

    Refactored tests to reduce code duplication

--- pycmakeserver/test/test_protocol.py
+++ pycmakeserver/test/test_protocol.py
@@ -5,6 +5,15 @@ class CmakeProtocolTest(TestCase):
     def setUp(self):
         self.handler = mock.Mock(['handle', 'connection_lost'])
         self.proto = CmakeProtocol(self.handler)
+        self.hello = b'''\n[== "CMake Server" ==[\n
+            { "type": "hello" }
+            \n]== "CMake Server" ==]\n'''
+        
+    def error_recovery_test(self, data_bad):
+        ''' Sends a bad message, then a good one, and asserts for recovery '''
+        self.proto.data_received(data_bad)
+        self.proto.data_received(self.hello)
+        self.handler.handle.assert_called_once_with({'type': 'hello'})
     
     # coonnection_lost
     def test_connection_lost_calls_handler_connection_lost(self):
@@ -14,27 +23,19 @@ class CmakeProtocolTest(TestCase):
     
     # data_received success cases
     def test_data_received_exactly_one_msg_decodes_calls_handler_handle(self):
-        data = b'''\n[== "CMake Server" ==[\n
-            { "type": "hello" }
-            \n]== "CMake Server" ==]\n'''
-        self.proto.data_received(data)
+        self.proto.data_received(self.hello)
         self.handler.handle.assert_called_once_with({'type': 'hello'})
         
     def test_data_received_message_in_two_pieces_decodes_calls_handle(self):
-        data = b'''\n[== "CMake Server" ==[\n
-            { "type": "hello" }
-            \n]== "CMake Server" ==]\n'''
-        self.proto.data_received(data[:10])
-        self.proto.data_received(data[10:])
+        self.proto.data_received(self.hello[:10])
+        self.proto.data_received(self.hello[10:])
         self.handler.handle.assert_called_once_with({'type': 'hello'})
         
     def test_data_received_two_messages_at_once_decodes_calls_handle_for_the_two(self):
-        data = b'''\n[== "CMake Server" ==[\n
-            { "type": "hello" }
-            \n]== "CMake Server" ==]\n\n[== "CMake Server" ==[\n
+        additional_data = b'''\n[== "CMake Server" ==[\n
             { "type": "signal" }
             \n]== "CMake Server" ==]\n'''
-        self.proto.data_received(data)
+        self.proto.data_received(self.hello + additional_data)
         expected_calls = [
             mock.call({'type': 'hello'}),
             mock.call({'type': 'signal'})
@@ -42,9 +43,7 @@ class CmakeProtocolTest(TestCase):
         self.assertEqual(self.handler.handle.mock_calls, expected_calls)
         
     def test_data_received_complete_message_and_incomplete_calls_handle_retains_incomplete(self):
-        data0 = b'''\n[== "CMake Server" ==[\n
-            { "type": "hello" }
-            \n]== "CMake Server" ==]\n\n[== "CMake Server" ==[\n'''
+        data0 = self.hello + b'''\n[== "CMake Server" ==[\n'''
         data1 = b'''{ "type": "signal" }
             \n]== "CMake Server" ==]\n'''
         self.proto.data_received(data0)
@@ -67,66 +66,37 @@ class CmakeProtocolTest(TestCase):
         data_bad = b'''\n[== "Bad" ==[\n
             { "type": "hello" }
             \n]== "CMake Server" ==]\n'''
-        data_ok = b'''\n[== "CMake Server" ==[\n
-            { "type": "hello" }
-            \n]== "CMake Server" ==]\n'''
-        self.proto.data_received(data_bad)
-        self.proto.data_received(data_ok)
-        self.handler.handle.assert_called_once_with({'type': 'hello'})
+        self.error_recovery_test(data_bad)
         
     def test_data_received_message_does_not_start_with_adequate_prefix_equal_length_discards(self):
         data_bad = b'''\n[== "CMake Serve_" ==[\n
             { "type": "hello" }
             \n]== "CMake Server" ==]\n'''
-        data_ok = b'''\n[== "CMake Server" ==[\n
-            { "type": "hello" }
-            \n]== "CMake Server" ==]\n'''
-        self.proto.data_received(data_bad)
-        self.proto.data_received(data_ok)
-        self.handler.handle.assert_called_once_with({'type': 'hello'})
+        self.error_recovery_test(data_bad)
         
     def test_data_received_message_does_not_start_with_adequate_prefix_longer_discards(self):
         data_bad = b'''\n[== "Invalid Very Long Message Head" ==[\n
             { "type": "hello" }
             \n]== "CMake Server" ==]\n'''
-        data_ok = b'''\n[== "CMake Server" ==[\n
-            { "type": "hello" }
-            \n]== "CMake Server" ==]\n'''
-        self.proto.data_received(data_bad)
-        self.proto.data_received(data_ok)
-        self.handler.handle.assert_called_once_with({'type': 'hello'})
+        self.error_recovery_test(data_bad)
         
     def test_data_received_incomplete_prefix_does_not_discard(self):
         # Bad prefix recovery should not discard good but incomplete prefixes
-        data = b'''\n[== "CMake Server" ==[\n
-            { "type": "hello" }
-            \n]== "CMake Server" ==]\n'''
-        self.proto.data_received(data[:5]) # Prefix is OK but incomplete
-        self.proto.data_received(data[5:])
+        self.proto.data_received(self.hello[:5]) # Prefix is OK but incomplete
+        self.proto.data_received(self.hello[5:])
         self.handler.handle.assert_called_once_with({'type': 'hello'})
         
     def test_data_received_invalid_utf8_discards(self):
         data_bad = b'''\n[== "CMake Server" ==[\n
             { "type": "\9cinvalid" }
             \n]== "CMake Server" ==]\n'''
-        data_ok = b'''\n[== "CMake Server" ==[\n
-            { "type": "hello" }
-            \n]== "CMake Server" ==]\n'''
-        self.proto.data_received(data_bad)
-        self.proto.data_received(data_ok)
-        self.handler.handle.assert_called_once_with({'type': 'hello'})
+        self.error_recovery_test(data_bad)
         
     def test_data_received_invalid_json_discards(self):
         data_bad = b'''\n[== "CMake Server" ==[\n
             { "type": "hello" ]
             \n]== "CMake Server" ==]\n'''
-        data_ok = b'''\n[== "CMake Server" ==[\n
-            { "type": "hello" }
-            \n]== "CMake Server" ==]\n'''
-        self.proto.data_received(data_bad)
-        self.proto.data_received(data_ok)
-        self.handler.handle.assert_called_once_with({'type': 'hello'})
+        self.error_recovery_test(data_bad)
     
-    # data received
     # write => writes prefix, suffix, serializes, writes to transport
     # close => Calls transport close
